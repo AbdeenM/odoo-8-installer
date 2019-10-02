9a6beeabcbae89ad9dd309f0c3c4611dbca4a5fe
Abdeen <abdeen.mohamed@outlook.com>
2019-10-02 20:00:50 +0000

    Update odoo-8-installer.sh

--- odoo-8-installer.sh
+++ odoo-8-installer.sh
@@ -7,14 +7,7 @@
 # This script will install Odoo on your Ubuntu 14.04 server. It can install multiple Odoo instances
 # in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
-# Make a new file:
-# sudo nano odoo-install.sh
-# Place this content in it and then make the file executable:
-# sudo chmod +x odoo-install.sh
-# Execute the script to install Odoo:
-# ./odoo-install
-################################################################################
- 
+
 ##fixed parameters
 #odoo
 OE_USER="odoo"
